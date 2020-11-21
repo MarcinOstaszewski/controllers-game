be068dd0c73ad9f3408f81baca17992c20864c23
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-11-21 17:00:44 +0100

    add creating cars by js, moving cars, display speed

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,40 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let gamepadInfo = document.getElementById("gamepad-info");
-    let buttonsInfo = document.getElementById("buttons-info");
     let dirInfo = document.getElementById("dir-info");
-    let direction = document.getElementsByClassName("direction");
-    let cars = document.getElementById("ship");
+    let cars = {};
+    const speed = 2;
+    let scrW = window.innerWidth;
+    let scrH = window.innerHeight;
+    let positions = {
+        0: [scrW / 3, scrH / 3],
+        1: [scrW / 3 * 2, scrH / 3 * 2],
+        2: [scrW / 3, scrH / 3 * 2],
+        3: [scrW / 3 * 2, scrH / 3],
+    }
+    let colors = {
+        0: "#3c66f1",
+        1: "#f1df3c",
+        2: "#3c66f1",
+        3: "#f13c4b",
+    }
     let start;
     let minAx = 0.0002
-    let dir;
     let pi = Math.PI;
-    let gpInfo = {};
 
-    function displayAllTextsFromObject (obj, elem) {
-        elem.innerHTML = "";
-        for (let ind in obj) {
-            elem.innerHTML += obj[ind];
-        }
+    createCar = (id) => {
+        cars[id] = document.createElement('div');
+        cars[id].setAttribute('id', id);
+        cars[id].classList.add('car');
+        console.log(id)
+        cars[id].style.left = positions[id][0];
+        cars[id].style.top = positions[id][1];
+        cars[id].style.backgroundColor = colors[id];
+        document.getElementById('container').appendChild(cars[id])
     }
 
     window.addEventListener("gamepadconnected", function (e) {
         let gp = e.gamepad;
-        gpInfo[gp.index] = "Gamepad connected at index " + gp.index + ". Buttons: " + gp.buttons.length + ". Axes " + gp.axes.length + "<br>";
-        displayAllTextsFromObject(gpInfo, gamepadInfo)
-        console.log(gp)
+        createCar(gp.index);
         gameLoop();
     });
 
     window.addEventListener("gamepaddisconnected", function (e) {
         let gp = e.gamepad;
         console.log(gp)
-        if (gpInfo[gp.index]) {
-            delete gpInfo[gp.index];
-            displayAllTextsFromObject(gpInfo, gamepadInfo)
-        } else {
-            gamepadInfo.innerHTML = "Waiting for gamepad.";
-        }
     });
 
     function gameLoop() {
@@ -42,45 +48,36 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!gamepads) return;
 
         var gp = {};
-        var gpStatusText = "";
         for (let i = 0; i < 4; i++) {
-            gpStatusText += "<br/><br/>GP" + i + ": buttons pressed: ";
             gp[i] = gamepads[i];
             if (gp[i]) {
-                for (let j = 0; j < gp[i].buttons.length; j++) {
-                    if (gp[i].buttons[j].pressed) {
-                        gpStatusText += j + ", ";
-                    }
-                }
+                let dir = -500;
                 let x0 = gp[i].axes[0];
                 let y0 = gp[i].axes[1];
-                let x1 = gp[i].axes[2];
-                let y1 = gp[i].axes[3];
                 let force = 0;
-                gpStatusText += "<br/> axes: x0: " + x0.toFixed(5) + ", y0: " + y0.toFixed(5) + ", " + x1.toFixed(5) + ", " + y1.toFixed(5) + "<br/>";
 
                 if (Math.abs(x0) > minAx || Math.abs(y0) > minAx) {
                     force = ( Math.abs(y0) + Math.abs(x0) ) - Math.abs(x0 * y0);
-                }
-
-                dirInfo.innerText = (x0 / y0);
-                if (Math.abs(force) > minAx * 2) {
-
-                    if (x0 > minAx && y0 < minAx || x0 < minAx && y0 < minAx) {
-                        if (x0 / y0 !== -1) {
+                    
+                    if (Math.abs(force) > minAx * 2) {
+                        if (x0 > minAx && y0 < minAx || x0 < minAx && y0 < minAx) {
                             dir = -( Math.atan(x0 / y0) / pi * 180 );
+                        } else if (x0 < minAx && y0 > minAx || x0 > minAx && y0 > minAx) {
+                            dir = 180 - ( Math.atan(x0 / y0) / pi * 180 );
+                        } 
+                        if (dir > -500) {
+                            cars[i].style.transform = "rotate(" + parseInt(dir) + "deg)";
+                            dirInfo.innerHTML = parseFloat(cars[i].style.left) + " ___TOP: " + parseFloat(cars[i].style.top);
+                            dirRad = dir * (pi / 180);
+                            cars[i].style.left = (parseFloat(cars[i].style.left) + (Math.sin(dirRad) * force) * speed) + "px"; 
+                            cars[i].style.top = (parseFloat(cars[i].style.top) - (Math.cos(dirRad) * force) * speed) + "px"; 
                         }
-                    } else if (x0 < minAx && y0 > minAx || x0 > minAx && y0 > minAx) {
-                        dir = 180 - ( Math.atan(x0 / y0) / pi * 180 );
-                    } 
+                    }
+                    
                 }
-                gpStatusText += "Force: " + force + "<br/>angle: " + dir;
-                if (dir) {
-                    direction[i].style.transform = "rotate(" + dir + "deg)";
-                }
+
             }
         }
-        buttonsInfo.innerHTML = gpStatusText;
 
         start = window.requestAnimationFrame(gameLoop);
     };
